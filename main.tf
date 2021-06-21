94140b975970d114f7fd6772390be4556a750015
Daniel Mease <daniel.mease@cloudacademy.com>
2021-06-21 08:58:56 -0600

    Forked repo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "wp-infrastructure" {
-  source  = "artibyrd/wp-infrastructure/gcp"
+  source  = "cloudacademy/wp-infrastructure/gcp"
   version = "1.2.0"
   project_id = "terraform-demo-309512"
   region1 = "us-west1"
